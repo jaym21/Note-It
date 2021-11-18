81a06defa59339af834d75e6e22e409dfe013925
jaym21 <jaynmistry21@gmail.com>
2021-11-18 16:14:29 +0530

    updated layout

--- app/src/main/java/dev/jaym/noteit/ui/AddNote.kt
+++ app/src/main/java/dev/jaym/noteit/ui/AddNote.kt
@@ -3,6 +3,7 @@ package dev.jaym.noteit.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.snackbar.Snackbar
 import dev.jaym.noteit.R
@@ -31,7 +32,10 @@ class AddNote : AppCompatActivity() {
 
                 //getting a random color for background
                 val colorsArray = resources.getIntArray(R.array.cardColors)
-                val randomColor =  colorsArray[(0..9).random()]
+                val randomInt = (0..9).random()
+                Log.d("TAGYOYO", "RANDOM COLOR $randomInt")
+                val randomColor =  colorsArray[randomInt]
+                Log.d("TAGYOYO", "RANDOM COLOR $randomColor")
                 viewModel.addNote(Note(0, binding?.etNoteTitle?.text.toString(), binding?.etNoteDes?.text.toString(), randomColor))
                 val mainIntent = Intent(this, MainActivity::class.java)
                 startActivity(mainIntent)
