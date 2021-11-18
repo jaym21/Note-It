0a6635b9585b85efe16afcac4eb629040fb63694
jaym21 <jaynmistry21@gmail.com>
2021-11-18 15:20:00 +0530

    implementing swipe to delete note

--- app/src/main/java/dev/jaym/noteit/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym/noteit/ui/MainActivity.kt
@@ -1,31 +1,23 @@
 package dev.jaym.noteit.ui
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.animation.AccelerateDecelerateInterpolator
 import android.view.animation.AnimationUtils
-import androidx.core.view.isVisible
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.*
 import dev.jaym.noteit.adapter.INoteRVAdapter
 import dev.jaym.noteit.adapter.NoteRVAdapter
 import dev.jaym.noteit.data.Note
-import com.google.android.material.snackbar.Snackbar
 import dev.jaym.noteit.R
 import dev.jaym.noteit.databinding.ActivityMainBinding
-import dev.jaym.noteit.extensions.startAnimation
 
 class MainActivity : AppCompatActivity(), INoteRVAdapter {
 
     private var binding: ActivityMainBinding? = null
     lateinit var viewModel: NoteViewModel
-    lateinit var adapter: NoteRVAdapter
+    lateinit var noteAdapter: NoteRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,60 +32,59 @@ class MainActivity : AppCompatActivity(), INoteRVAdapter {
             startActivity(addNoteIntent)
         }
 
-
-
         //initializing recyclerView
         binding?.recyclerView?.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
 
         //initializing adapter
-        adapter = NoteRVAdapter(this, this)
+        noteAdapter = NoteRVAdapter(this, this)
 
         //passing adapter to recyclerView
-        binding?.recyclerView?.adapter = adapter
+        binding?.recyclerView?.adapter = noteAdapter
+
+        setUpSwipeToDeleteItem()
 
         //creating a instance or object of viewModel
         viewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
 
         viewModel.allNotes.observe(this, Observer {list -> list?.let {
             //checking if the list is null that is no change is observed then this function won't get executed
-            adapter.updateList(it)
+            noteAdapter.submitList(it)
         }
         })
 
 
-        //implementing swipe to delete
-        val itemTouchHelper = object: ItemTouchHelper.SimpleCallback(
+    }
+
+    private fun setUpSwipeToDeleteItem() {
+        val swipeToDelete = object : ItemTouchHelper.SimpleCallback(
             ItemTouchHelper.UP or ItemTouchHelper.DOWN,
-            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-        ) {
-            override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
+            ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
                 //this will be empty as on up and down movement we do not need to do any thing with note
                 return true
             }
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                //getting the position of the item swiped
-                val position = viewHolder.adapterPosition
-                //now getting the note from database using position in RV
-                val note = adapter.getNote(position)
-                //deleting the note using viewModel function
-                viewModel.deleteNote(note)
-                //making a snackbar to show that note is deleted and giving an option to undo the delete
-                Snackbar.make(binding?.root!!, "Note Deleted", Snackbar.LENGTH_LONG).apply {
-                    setAction("Undo") {
-                        //saving back the note in database that was deleted
-                        viewModel.insertNote(note)
-                    }
-                    show()
-                }
+                //getting position of item which is swiped
+                val itemPosition = viewHolder.adapterPosition
+                //getting currentList
+                val currentList =  noteAdapter.currentList.toMutableList()
+                //getting the swiped item
+                val swipedItem = currentList[itemPosition]
+                //removing item from list
+                currentList.removeAt(itemPosition)
+
+                //removing from database
+                viewModel.deleteNote(swipedItem)
+
+                noteAdapter
             }
-        }
 
-        //making the itemTouchHelper and attaching to recyclerView
-        ItemTouchHelper(itemTouchHelper).apply {
-            attachToRecyclerView(binding?.recyclerView)
         }
-
     }
 
     private fun runRecyclerViewAnimation(recyclerView: RecyclerView) {
@@ -103,39 +94,15 @@ class MainActivity : AppCompatActivity(), INoteRVAdapter {
         recyclerView.scheduleLayoutAnimation()
     }
 
-    override fun onStart() {
-        super.onStart()
-        runRecyclerViewAnimation(binding?.recyclerView!!)
-    }
-
     override fun onResume() {
         super.onResume()
-        binding?.root?.isVisible = true
         runRecyclerViewAnimation(binding?.recyclerView!!)
     }
-
-    override fun onRestart() {
-        super.onRestart()
-        runRecyclerViewAnimation(binding?.recyclerView!!)
-    }
-
 
     override fun onCardClicked(note: Note) {
         //whenever a card of note is clicked we navigate to openNote activity passing the note
         val openNoteIntent = Intent(this, OpenNote::class.java)
         openNoteIntent.putExtra("SelectedNote", note)
         startActivity(openNoteIntent)
-    }
-
-
-    //making a fun to search database using query entered and updating the recyclerview with the result
-    private fun searchDatabase(searchQuery: String) {
-        val searchQuery = "%$searchQuery%" //% is added to format the sql query for searching
-
-        viewModel.searchNote(searchQuery).observe(this) {
-            it.let {
-                adapter.updateList(it)
-            }
-        }
     }
 }
